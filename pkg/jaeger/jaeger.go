c6d99b61f7656abde890f78b4510fa43f87084e5
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-27 15:32:17 +0300

    feat: add InitJaeger helper

---
+++ pkg/jaeger/jaeger.go
@@ -0,0 +1,31 @@
+package jaeger
+
+import (
+	"github.com/AleksK1NG/email-microservice/config"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+	"github.com/uber/jaeger-lib/metrics"
+	"io"
+)
+
+// Init Jaeger
+func InitJaeger(cfg *config.Config) (opentracing.Tracer, io.Closer, error) {
+	jaegerCfgInstance := jaegercfg.Configuration{
+		ServiceName: cfg.Jaeger.ServiceName,
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans:           cfg.Jaeger.LogSpans,
+			LocalAgentHostPort: cfg.Jaeger.Host,
+		},
+	}
+
+	return jaegerCfgInstance.NewTracer(
+		jaegercfg.Logger(jaegerlog.StdLogger),
+		jaegercfg.Metrics(metrics.NullFactory),
+	)
+}
