f4edde6014eaa6214a783fb8d966284a751238f7
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-28 15:40:12 +0300

    feat: add FindEmailById email gRPC handler

--- internal/email/delivery/grpc/email_microservice.go
+++ internal/email/delivery/grpc/email_microservice.go
@@ -8,8 +8,10 @@ import (
 	"github.com/AleksK1NG/email-microservice/internal/models"
 	"github.com/AleksK1NG/email-microservice/pkg/grpc_errors"
 	"github.com/AleksK1NG/email-microservice/pkg/logger"
+	"github.com/google/uuid"
 	"github.com/opentracing/opentracing-go"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // Email gRPC microservice
@@ -54,5 +56,29 @@ func (e *EmailMicroservice) FindEmailById(ctx context.Context, r *emailService.F
 	span, ctx := opentracing.StartSpanFromContext(ctx, "EmailUseCase.FindEmailById")
 	defer span.Finish()
 
-	return &emailService.FindEmailByIdResponse{Email: nil}, nil
+	emailUUID, err := uuid.Parse(r.GetEmailUuid())
+	if err != nil {
+		e.logger.Errorf("uuid.Parse: %v", err)
+		return nil, status.Errorf(grpc_errors.ParseGRPCErrStatusCode(err), "uuid.Parse: %v", err)
+	}
+
+	emailById, err := e.emailUC.FindEmailById(ctx, emailUUID)
+	if err != nil {
+		e.logger.Errorf("emailUC.FindEmailById: %v", err)
+		return nil, status.Errorf(grpc_errors.ParseGRPCErrStatusCode(err), "emailUC.FindEmailById: %v", err)
+	}
+
+	return &emailService.FindEmailByIdResponse{Email: e.convertEmailToProto(emailById)}, nil
+}
+
+func (e *EmailMicroservice) convertEmailToProto(email *models.Email) *emailService.Email {
+	return &emailService.Email{
+		EmailId:     email.EmailID.String(),
+		To:          email.To,
+		From:        email.From,
+		Body:        email.Body,
+		Subject:     email.Subject,
+		ContentType: email.ContentType,
+		CreatedAt:   timestamppb.New(email.CreatedAt),
+	}
 }
